c129fc968507cfbcb163fd7ac165876042a245c2
Tyvan Cheng <tyvancheng@gmail.com>
2023-06-10 17:24:27 -0400

    create user works

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,17 @@
-const express = require('express');
+const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const debug = require('debug');
+
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
+
+require('./models/User');
 
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
@@ -12,8 +20,49 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// ATTACH EXPRESS ROUTERS HERE
+// Security Middleware
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, React files
+    // will be served statically on the Express server.)
+    app.use(cors());
+}
+
+// Set the _csrf token and create req.csrfToken method to generate a hashed
+// CSRF token
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
+
+// Attach Express routers
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
+app.use('/api/csrf', csrfRouter);
 
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        statusCode,
+        errors: err.errors
+    })
+});
 module.exports = app;
