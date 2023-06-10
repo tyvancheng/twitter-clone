c14379cd81d80a4f3c2e32ee96e4a9b6a144f437
Tyvan Cheng <tyvancheng@gmail.com>
2023-06-10 18:02:34 -0400

    auth works

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,3 +1,4 @@
+
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const bcrypt = require('bcryptjs');
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -55,4 +55,17 @@ router.post('/register', async (req, res, next) => {
     })
   });
 });
+
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('local', async function(err, user) {
+    if (err) return next(err);
+    if (!user) {
+      const err = new Error('Invalid credentials');
+      err.statusCode = 400;
+      err.errors = { email: "Invalid credentials" };
+      return next(err);
+    }
+    return res.json({ user });
+  })(req, res, next);
+});
 module.exports = router;
